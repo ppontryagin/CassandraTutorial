13e99571cafe7aac99e2eb73ed9d929cb20ef29f
Pavel Pontryagin <pavel.pontryagin@billing.ru>
2014-12-01 14:31:21 +0300

    Added TweetDAO

---
+++ src/main/java/com/peterservice/cassandratutorial/ch1/TweetDAO.java
@@ -0,0 +1,16 @@
+package com.peterservice.cassandratutorial.ch1;
+
+public class TweetDAO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	/**
+	 * @author Pavel.Pontryagin
+	 *
+	 */
+}
